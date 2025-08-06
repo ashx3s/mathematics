c0652286d7669d7d1f109aa588156b1238c25379
ashx3s <ashx3s@tuta.io>
2025-08-06 10:37:38 -0600

    spelling fix and better error handle n

--- src/binomialTheorem.ts
+++ src/binomialTheorem.ts
@@ -1,17 +1,17 @@
 import nChooseK from "./nChooseK";
 
 function binomialTheorem(a: number, b: number, n: number): number {
-  if (!a || !b || !n) {
-    throw new Error("All parameters must be non-zero");
+  if (n < 0 || !Number.isInteger(n)) {
+    throw new Error("the exponent n must be an integer greater than 0");
   }
 
   const terms: number[] = [];
   for (let k = 0; k <= n; k++) {
-    const coeffecient = nChooseK(n, k);
+    const coefficient = nChooseK(n, k);
     const aExponent = n - k;
     const aValue = Math.pow(a, aExponent);
     const bValue = Math.pow(b, k);
-    const term = coeffecient * aValue * bValue;
+    const term = coefficient * aValue * bValue;
     terms.push(term);
   }
   return terms.reduce((acc, curr) => acc + curr);
