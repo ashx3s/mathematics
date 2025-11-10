4f18b63ea8f90a89c5433c311b66b10ea4d2517e
Ashlyn Knox <ashx3s@tuta.io>
2025-11-09 22:36:48 -0700

    factorial code cleanup

--- src/factorial.ts
+++ src/factorial.ts
@@ -3,6 +3,5 @@ export default function factorial(n: number) {
     throw new Error("Factorial is only defined for non-negative integers");
   }
   if (n === 0) return 1;
-  // create an array from the number and add the values to it
-  return Array.from({ length: n }, (_, k) => k + 1).reduce((acc, curr) => acc * curr, 1);
+  return Array.from({ length: n }, (_, k) => k + 2).reduce((acc, curr) => acc * curr, 1);
 }
