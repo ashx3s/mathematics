6d5d13189776f2873ef5017e3818a3d282d26fca
Ashlyn Knox <ashx3s@tuta.io>
2025-11-09 22:08:54 -0700

    functional refactor

--- src/binomialTheorem.ts
+++ src/binomialTheorem.ts
@@ -5,14 +5,9 @@ function binomialTheorem(a: number, b: number, n: number): number {
     throw new Error("the exponent n must be an integer greater than 0");
   }
 
-  let sum = 0;
-  for (let k = 0; k <= n; k++) {
-    const coefficient = nChooseK(n, k);
-    const aExponent = n - k;
-    const aValue = Math.pow(a, aExponent);
-    const bValue = Math.pow(b, k);
-    sum += coefficient * aValue * bValue;
-  }
+  const sum = Array.from({ length: n + 1 }, (_, k) => k)
+    .map(k => nChooseK(n, k) * Math.pow(a, n - k) * Math.pow(b, k))
+    .reduce((acc, term) => acc + term, 0);
   return sum;
 }
 
